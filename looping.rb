3f1742bf7093b2facc82aa68d4ddd0c43d09d8ca
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-08-16 21:27:46 +0300

    commit tree ca0a60f017f5dd969a68a19561f317b25b2a5f15

--- looping.rb
+++ looping.rb
@@ -1,6 +1,13 @@
+# #happy_new_year
+# puts out a countdown from 10 to 1, followed by "Happy New Year!
+
 def happy_new_year
-  # your code here
+  10.downto(1) do |i|
+    puts i
+  end
+  puts "Happy New Year!"
 end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +22,26 @@ def fizzbuzz(num)
   end
 end
 
+#fizzbuzz_printer
+# Write a method `fizzbuzz_printer` that prints the numbers from 1 to 100. For
+# multiples of three, print "Fizz" instead of the number and for the multiples
+# of five print "Buzz". For numbers which are multiples of both three and five,
+# print "FizzBuzz".
+
+
 def fizzbuzz_printer
-  # your code here
+  1.upto(100) do |i|
+    puts fizzbuzz(i)
+  end
 end
 
-def reverse_string(str)
-  # your code here
+
+
+def reverse_string(string)
+  reversed_string = ""
+  string.length.times do |i|
+    reversed_string << string[-i-1]
+  end
+  reversed_string
 end
+
